cdebfce0514d0f732d3bc2fef8474028594f4fca
user <user@211.48.213.162>
2023-02-06 10:47:39 +0900

    오라클 계정 수정

--- racing/src/DAO/JoinDAO.java
+++ racing/src/DAO/JoinDAO.java
@@ -18,7 +18,7 @@ public class JoinDAO {
 			try {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 				String url = "jdbc:oracle:thin:@localhost:1521:xe";
-				String db_id = "system";
+				String db_id = "c##horsegame";
 				String db_pw = "12345";
 				conn = DriverManager.getConnection(url, db_id,db_pw);
 				
