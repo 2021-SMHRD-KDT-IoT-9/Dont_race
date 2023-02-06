5dc645be4d990cc9658a2cb7b9e62a0f707280d5
hyunsoloh <77195736+hyunsoloh@users.noreply.github.com>
2023-02-06 11:27:22 +0900

    커밋

--- racing/src/model/MyHorse.java
+++ racing/src/model/MyHorse.java
@@ -1,17 +1,39 @@
 package model;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class MyHorse implements Horse{
+	private int max;
+	private int min;
+	private String line = ""; 
+	
+	public MyHorse(int max, int min) {
+		super();
+		this.max = max;
+		this.min = min;
+	}
 
 	@Override
 	public String name() {
 		// TODO Auto-generated method stub
-		return null;
+		return HorseInfo.getName;
 	}
 
 	@Override
 	public String move() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Scanner sc = new Scanner(System.in);
+		Random rm = new Random();
+		String input = sc.nextLine();
+		if(input.equals("")){
+			int ran = 0;
+			ran = rm.nextInt(max);
+			for(int i =min; i< ran; i++){
+				line+="-";
+			}
 	
+		}
+		return line;
+	}
+
 }
